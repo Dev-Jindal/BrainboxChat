e96838f84c336c40b75894bb6faa700bba6852da
Dev-Jindal <168623305+Dev-Jindal@users.noreply.github.com>
2025-07-12 00:24:17 +0530

    Update auth.middleware.js

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -3,7 +3,8 @@ import redisClient from "../services/redis.service.js";
 export const authUser = async (req, res, next) => {
     try {
         const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ];
-
+        console.log("token is",token);
+         console.log(req.headers.authorization.split(' ')[ 1 ]);
         if (!token) {
             return res.status(401).send({ error: 'Unauthorized User' });
         }
